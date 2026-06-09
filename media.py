2c25c00d5258368dabeab5372193b446d087d703
Guilherme de Souza Santos <guilherme.santos61@aluno.cps.sp.gov.br>
2026-06-09 11:47:22 -0300

    Versão Inicial

--- media.py
+++ media.py
@@ -5,7 +5,7 @@ def main():
     a = int(input('1 Valor? '))
     b = int(input('2 valor? '))
     c = int(input('3 valor? '))
-    print(f'A média e {media(a, b, c): 2.f}')
+    print(f'A média e {(media(a, b, c}: 2.f)')
 
 
 if __name__ == '__main__':
